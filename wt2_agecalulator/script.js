3f61d2ed5250e57db3c4e62eefab3c74d6a69ce0
kunalmittal01 <kunalmittalmrt@gmail.com>
2024-07-28 18:17:40 +0530

    age calculator modi.

--- wt2_agecalulator/script.js
+++ wt2_agecalulator/script.js
@@ -8,31 +8,28 @@ btn.addEventListener('click', function(e){
     let today = new Date();
     let birthDate = new Date(dob.value);
     let birthd = birthDate.toString().split(' ')[2];
-    let birthm = birthDate.toString().split(' ')[1];
+    let birthm = birthDate.getMonth() + 1;
     let birthy = birthDate.toString().split(' ')[3];
 
-    let todayd = today.toString().split(' ')[2];
-    let todaym = today.toString().split(' ')[1];
-    let todayy = today.toString().split(' ')[3];
+    let todayd = Number(today.toString().split(' ')[2]);
+    let todaym = today.getMonth() + 1;
+    let todayy = Number(today.toString().split(' ')[3]);
 
-    let res = [];
-    if(Number(todayd) < Number(birthd)){
-        let v1 = Number(todayd);
-        let v2 = Number(birthd);
-        res.push(v1+30 - v2);
+    let res;
+    if(todayd < birthd){
+        let v1 = todayd;
+        let v2 = birthd;
         todaym--;
     }
-    if(Number(todaym) < Number(birthm)){
-        let v1 = Number(todaym);
-        let v2 = Number(birthm);
-        res.push(v1 + 12 - v2);
-        todayy--;
+    if(todaym < birthm){
+        let v1 = todaym;
+        let v2 = birthm;
+        todayy-=1;
     }
-    res.push(Number(todayy) - Number(birthy));
-    let out = (res.length == 1)?res[0]:res[2];
-    if(isNaN(out)){
+    res = todayy - birthy;
+    if(isNaN(res)){
         alert("Please enter birth date");
         return;
     }
-    disp.innerHTML = `Your age is ${out} years.`;
+    disp.innerHTML = `Your age is ${res} years.`;
 });
