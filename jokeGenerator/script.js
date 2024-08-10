23268d29d2428611885a33fbd0b4260c8d88703a
kunalmittal01 <kunalmittalmrt@gmail.com>
2024-08-10 17:51:59 +0530

    jokeGenerator added

---
+++ jokeGenerator/script.js
@@ -0,0 +1,43 @@
+let btn = document.getElementById('jokebtn');
+let disp = document.querySelector('.disp');
+let h1 = document.querySelector('h1');
+let toggle = document.getElementById('toggle');
+
+let fetchJoke = ()=> {
+    disp.innerHTML = 'updating...';
+    fetch('https://official-joke-api.appspot.com/jokes/random/250').then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        let joke = Math.floor(Math.random()*250);
+            disp.innerHTML = `<p>setup: ${data[joke].setup}<br><br>punchline: ${data[joke].punchline}</p>`;
+
+     }).catch((err)=>console.log(err));
+
+}
+
+let randClr = setInterval(() => {
+    h1.style.color = '#' + Math.floor(Math.random()*16777215).toString(16);
+},1000);
+
+btn.addEventListener('click', fetchJoke);
+
+let tog = true, body = document.querySelector('body');
+toggle.addEventListener('click',()=>{
+    toggle.classList.toggle('change-mode');
+    if(tog) {
+        body.style = 'none';
+        body.style.backgroundImage = 'linear-gradient(to bottom, black,brown)';
+        tog = false;
+    }
+    else {
+        body.style = 'none';
+        body.style.backgroundImage = 'linear-gradient(to left bottom, lightblue, lightpink, lightblue)';
+        tog = true;
+    }
+});
+
+let font = document.getElementById('selectfont');
+
+font.addEventListener('change',()=>{
+    disp.style.fontSize = `${font.value}px`;
+});
