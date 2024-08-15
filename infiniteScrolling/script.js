d3425e4b49d50bed76c562e6056770b64f365eb6
kunalmittal01 <kunalmittalmrt@gmail.com>
2024-08-15 18:50:22 +0530

    infinite_scroll added

---
+++ infiniteScrolling/script.js
@@ -0,0 +1,33 @@
+let disp = document.getElementsByClassName('cont')[0];
+let count = 10;
+const api_key = 'DDIVJSgdK-GI1wA3aHOtxC9YTt8tCY6-4jMk7guznY';
+async function fetchImages() {
+    let data = await fetch(`https://api.unsplash.com/photos/random/?client_id=_${api_key}&count=${count}`);
+
+    let image = await data.json();
+    console.log(image);
+    
+    return image;
+}
+
+async function displayImage() {
+    let data = await fetchImages();
+    disp.innerHTML = data.map((image)=>{
+        let div = document.createElement('div');
+        let div2 = document.createElement('div');
+        div2.appendChild(div);
+        div.classList.add('image');
+        div.innerHTML = 
+        `<img src="${image.urls.full}">
+    `
+         return div2.innerHTML;
+    }).join('');
+}
+displayImage();
+
+window.onscroll = function() {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        count += 3;
+        displayImage();
+    }  // End if  scroll to bottom of the page
+}
